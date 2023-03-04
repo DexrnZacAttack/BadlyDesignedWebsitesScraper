abc0468807b40e0b678d648d489c1b32198e62d0
ZacAttack <60078656+DexrnZacAttack@users.noreply.github.com>
2023-03-04 14:30:05 -0800

    Inital
    holy shit i got it working

---
+++ FridayHarborScraper.ps1
@@ -0,0 +1,10 @@
+﻿$documentid = 1
+
+do {
+    $url = "http://fridayharbor.org/imagerepository/document?documentid=$documentid"
+    $response = Invoke-WebRequest -Uri $url
+    $extension = $response.Headers["Content-Type"].Split("/")[-1]
+    $filename = "document_$documentid.$extension"
+    Invoke-WebRequest -Uri $url -OutFile $filename
+    $documentid++
+} while ($true)
